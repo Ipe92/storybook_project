f63f002c4e990ee7eca1f8f9d4504692c49fb495
Ipe92 <Ipe92@users.noreply.github.com>
2021-10-15 15:13:35 +0300

    Added Button.tsx, cannot find label

---
+++ src/components/Button.tsx
@@ -0,0 +1,52 @@
+/*
+Step 5: Test Props
+Storybook can't load docgen information from pre-built libraries,
+so to test that aspect out, create a file ./src/stories/Button.tsx:
+*/
+// type ButtonProps = {
+// 	children: ReactNode;
+
+// 	/**
+// 	 * Simple click handler
+// 	 */
+// 	onClick?: () => void;
+// };
+
+/**
+ * The world's most _basic_ button
+//  */
+// export const Button: FC<ButtonProps> = ({ children, onClick }: ButtonProps) => (
+// 	<button onClick={onClick} type="button">
+// 		{children}
+// 	</button>
+// );
+
+interface ButtonProps {
+	label?: string;
+	backgroundColor?: string;
+	size?: "sm" | "md" | "lg";
+	handleClick?(...args: unknown[]): unknown;
+}
+
+function Button({
+	label: any,
+	backgroundColor = "red",
+	size = "md",
+	handleClick,
+}: ButtonProps) {
+	let scale = 1;
+	if (size === "sm") scale = 0.75;
+	if (size === "lg") scale = 1.75;
+	const style = {
+		backgroundColor,
+		padding: `${scale * 0.5}rem ${scale * 1}rem`,
+		border: "none",
+	};
+	return (
+		<button onClick={handleClick} style={style}>
+			{label}
+		</button>
+	);
+}
+
+export default Button;
