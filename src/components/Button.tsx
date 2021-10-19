9a7fb917569a3691999668ce3e91594906b4e9aa
Ipe92 <Ipe92@users.noreply.github.com>
2021-10-19 09:28:25 +0300

    deleted useless comment

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,25 +1,4 @@
-/*
-Step 5: Test Props
-Storybook can't load docgen information from pre-built libraries,
-so to test that aspect out, create a file ./src/stories/Button.tsx:
-*/
-// type ButtonProps = {
-// 	children: ReactNode;
-
-// 	/**
-// 	 * Simple click handler
-// 	 */
-// 	onClick?: () => void;
-// };
-
-/**
- * The world's most _basic_ button
-//  */
-// export const Button: FC<ButtonProps> = ({ children, onClick }: ButtonProps) => (
-// 	<button onClick={onClick} type="button">
-// 		{children}
-// 	</button>
-// );
+// Step 5: Test Props
 
 export interface ButtonProps {
 	label?: string;
@@ -38,11 +17,13 @@ export function Button({
 	let scale = 1;
 	if (size === "sm") scale = 0.75;
 	if (size === "lg") scale = 1.75;
+
 	const style = {
 		backgroundColor,
 		padding: `${scale * 0.5}rem ${scale * 1}rem`,
 		border: "none",
 	};
+
 	return (
 		<button onClick={handleClick} style={style}>
 			{label}
