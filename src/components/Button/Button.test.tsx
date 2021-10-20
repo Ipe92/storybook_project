50141427f3c1ff5a0d7fc4d86043bf014924a027
Ipe92 <Ipe92@users.noreply.github.com>
2021-10-20 15:26:00 +0300

    Tests now use @storybook/testing-react

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -3,58 +3,45 @@
 // We really only need to test that the button gets rendered at all
 // (we don’t care about what the label says — it may say different things in different languages, depending on user locale settings).
 
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-import { Red, Green, Small, Large, LongLabel } from "./Button.stories";
+import { composeStories } from "@storybook/testing-react";
+import * as ButtonStories from "./Button.stories";
 
-beforeEach(() => {});
+const { Red, Green, Small, Large, LongLabel } = composeStories(ButtonStories);
 
 describe("red button", () => {
 	it("renders the button in the primary state", () => {
-		render(<Red {...Red.args} />);
-		expect(screen.getByRole("button")).toHaveTextContent("Press Me");
-	});
-
-	it("should have red background", () => {
-		render(<Red {...Red.args} />);
-		expect(screen.getByRole("button")).toHaveStyle({
-			backgroundColor: "red",
-		});
+		const { container } = render(<Red />);
+		expect(container).not.toBe(null);
 	});
 });
 
 describe("green button", () => {
 	it("renders the button in the primary state", () => {
-		render(<Green {...Green.args} />);
-		expect(screen.getByRole("button")).toHaveTextContent("Press Me");
-	});
-
-	it("should have green background", () => {
-		render(<Green {...Green.args} />);
-		expect(screen.getByRole("button")).toHaveStyle({
-			backgroundColor: "green",
-		});
+		const { container } = render(<Red />);
+		expect(container).not.toBe(null);
 	});
 });
 
 describe("Small button", () => {
 	it("renders the button in the primary state", () => {
-		render(<Small {...Small.args} />);
-		expect(screen.getByRole("button")).toHaveTextContent("Press Me");
+		const { container } = render(<Small />);
+		expect(container).not.toBe(null);
 	});
 });
 
 describe("Large button", () => {
 	it("renders the button in the primary state", () => {
-		render(<Large {...Large.args} />);
-		expect(screen.getByRole("button")).toHaveTextContent("Press Me");
+		const { container } = render(<Large />);
+		expect(container).not.toBe(null);
 	});
 });
 
-describe("Long label button", () => {
-	it("renders the button in the primary state", () => {
-		render(<LongLabel {...LongLabel.args} />);
-		expect(screen.getByRole("button")).toHaveTextContent("Press Me");
+describe("Testing Button with stories", () => {
+	it("Should not be null", () => {
+		const { container } = render(<LongLabel />);
+		expect(container).not.toBe(null);
 	});
 });
