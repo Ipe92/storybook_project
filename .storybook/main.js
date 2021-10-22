f46992a42d0f06b9273ad2099b3d2375ac2f05ec
Ipe92 <Ipe92@users.noreply.github.com>
2021-10-22 09:30:40 +0300

    @react-theming/storybook-addon commented out

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,12 @@
+// @react-theming/storybook-addon disables until themes work
+
 module.exports = {
 	stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@storybook/preset-create-react-app",
-		"@react-theming/storybook-addon",
+		// "@react-theming/storybook-addon",
 	],
 	typescript: {
 		check: false,
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,14 +3,17 @@ Some libraries require components higher up in the component hierarchy to render
 For example, in Styled Components, a ThemeProvider is necessary if your components use themes.
 Add a single global decorator that adds this context to all stories in .storybook/preview.js:
 */
-import { ThemeProvider } from "styled-components";
-import { addDecorator } from "@storybook/react";
-import { withThemes } from "@react-theming/storybook-addon";
 
-import { theme } from "../src/components/Button/Button";
+// @react-theming/storybook-addon disabled until I get themes to work
+// import { ThemeProvider } from "styled-components";
+// import { addDecorator } from "@storybook/react";
+// import { withThemes } from "@react-theming/storybook-addon";
+
+// import { theme } from "../src/components/Button/Button";
 
 // pass ThemeProvider and array of your themes to decorator
-addDecorator(withThemes(ThemeProvider, [theme]));
+// addDecorator(withThemes(ThemeProvider, [theme]));
+
 export const parameters = {
 	actions: { argTypesRegex: "^on[A-Z].*" },
 	controls: {
