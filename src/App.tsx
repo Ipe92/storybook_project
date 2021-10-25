334692c90f395f527fb0588c26d8dd67d3a54218
Ipe92 <Ipe92@users.noreply.github.com>
2021-10-25 10:48:40 +0300

    Added bright colors in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button/Button";
 import { lightTheme } from "./components/styles/lightTheme";
 import { darkTheme } from "./components/styles/darkTheme";
+import { brightColors } from "./components/styles/brightColors";
 
 function App() {
 	return (
@@ -14,6 +15,12 @@ function App() {
 				<ThemeProvider theme={darkTheme}>
 					<header className="App-header">
 						<h2>Button with dark theme</h2>
+						<Button />
+					</header>
+				</ThemeProvider>
+				<ThemeProvider theme={brightColors}>
+					<header className="App-header">
+						<h2>Button with bright colors theme</h2>
 						<Button />
 					</header>
 				</ThemeProvider>
