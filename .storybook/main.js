ff3cf70b1f66f2e00a9ca7eb98f8e08e44f2e5b5
Ipe92 <Ipe92@users.noreply.github.com>
2021-10-29 10:28:14 +0300

    Deleted unused storybook theme addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,8 +4,6 @@ module.exports = {
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@storybook/preset-create-react-app",
-		//"@react-theming/storybook-addon",
-		//"storybook-addon-styled-component-theme/dist/preset",
 		"themeprovider-storybook/register",
 	],
 	typescript: {
